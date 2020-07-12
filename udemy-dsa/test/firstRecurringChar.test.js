9cc839ba37a1e59c6346e9fd6f3ea2b2170bf9c4
Julian <jthrow00@gmail.com>
2020-07-12 00:36:30 -0700

    Wrote test cases for no recurring chars

--- udemy-dsa/test/firstRecurringChar.test.js
+++ udemy-dsa/test/firstRecurringChar.test.js
@@ -12,6 +12,14 @@ describe('firstRecurringChar()', () => {
       expect(firstRecurringChar([1, 2, 2, 3, 3, 3, 3, 4, 5, 1])).to.equal(2);
     });
   });
+  context('no recurring chars', () => {
+    it('should return undefined', () => {
+      expect(firstRecurringChar([1, 2, 3])).to.equal(undefined);
+    });
+  });
+});
+
+describe('firstRecurringChar2()', () => {
   context('only one recurring char', () => {
     it('should return recurring char', () => {
       expect(firstRecurringChar2([1, 2, 3, 4, 5, 1])).to.equal(1);
@@ -20,6 +28,11 @@ describe('firstRecurringChar()', () => {
   context('multiple recurring chars', () => {
     it('should return only first recurring char', () => {
       expect(firstRecurringChar2([1, 2, 2, 3, 3, 3, 3, 4, 5, 1])).to.equal(2);
+    });
+  });
+  context('no recurring chars', () => {
+    it('should return undefined', () => {
+      expect(firstRecurringChar2([1, 2, 3])).to.equal(undefined);
     });
   });
 });
