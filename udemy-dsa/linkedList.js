047912bcd9274343c6cb342142743b75c57018f8
Julian <jthrow00@gmail.com>
2020-07-12 02:46:10 -0700

    Implemented Linked List class append, prepend, insert, printList functions

---
+++ udemy-dsa/linkedList.js
@@ -0,0 +1,62 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const nodeToAppend = new Node(value);
+    this.tail.next = nodeToAppend;
+    this.tail = nodeToAppend;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const nodeToPrepend = new Node(value);
+    nodeToPrepend.next = this.head;
+    this.head = nodeToPrepend;
+    this.length++;
+    return this;
+  }
+
+  insert(index, value) {
+    if (index === 0) return this.prepend(value);
+    if (index >= this.length) return this.append(value);
+    const nodeToInsert = new Node(value);
+    let currentNode = this.head;
+    for (let i = 1; i < index; i++) {
+      currentNode = currentNode.next;
+    }
+    nodeToInsert.next = currentNode.next;
+    currentNode.next = nodeToInsert;
+    this.length++;
+  }
+
+  printList() {
+    const linkedListArr = [];
+    let currentNode = this.head;
+    while(currentNode !== null) {
+      linkedListArr.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return linkedListArr;
+  }
+}
+
+const ll = new LinkedList(10);
+ll.append(6);
+ll.append(1);
+ll.prepend(9001);
+ll.insert(3, 'woooot');
+ll.insert(0, 'front woo');
+ll.insert(100, 'back woo');
+console.log(ll.printList());
