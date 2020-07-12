ec9ad1d2ade4a0ba8dc73586cd6c060bb36df01e
Julian <jthrow00@gmail.com>
2020-07-12 00:33:39 -0700

    Wrote unit tests for firstRecurringChar and firstRecurringChar2 functions

---
+++ udemy-dsa/test/firstRecurringChar.test.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai');
+const { firstRecurringChar, firstRecurringChar2 } = require('../firstRecurringChar');
+
+describe('firstRecurringChar()', () => {
+  context('only one recurring char', () => {
+    it('should return recurring char', () => {
+      expect(firstRecurringChar([1, 2, 3, 4, 5, 1])).to.equal(1);
+    });
+  });
+  context('multiple recurring chars', () => {
+    it('should return only first recurring char', () => {
+      expect(firstRecurringChar([1, 2, 2, 3, 3, 3, 3, 4, 5, 1])).to.equal(2);
+    });
+  });
+  context('only one recurring char', () => {
+    it('should return recurring char', () => {
+      expect(firstRecurringChar2([1, 2, 3, 4, 5, 1])).to.equal(1);
+    });
+  });
+  context('multiple recurring chars', () => {
+    it('should return only first recurring char', () => {
+      expect(firstRecurringChar2([1, 2, 2, 3, 3, 3, 3, 4, 5, 1])).to.equal(2);
+    });
+  });
+});
