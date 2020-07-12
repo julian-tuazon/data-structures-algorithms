8b941b132359cccebef48f57471f2129e16272f6
Julian <jthrow00@gmail.com>
2020-07-11 17:35:12 -0700

    Created array implementation

---
+++ udemy-dsa/array.js
@@ -0,0 +1,39 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.data;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  deleteAtIndex(index) {
+    const deletedItem = this.data[index];
+    this.shiftItemsLeft(index);
+    return deletedItem;
+  }
+
+  shiftItemsLeft(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+exports.MyArray = MyArray;
