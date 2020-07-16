3c296b1f20451b734e994e239256cd0529eee2e0
Julian <jthrow00@gmail.com>
2020-07-15 23:36:04 -0700

    Removed unnecessary console logs

--- udemy-dsa/binarySearchTree.js
+++ udemy-dsa/binarySearchTree.js
@@ -57,26 +57,3 @@ class BinarySearchTree {
     }
   }
 }
-
-const tree = new BinarySearchTree();
-tree.insert(9)
-tree.insert(4)
-tree.insert(6)
-tree.insert(20)
-tree.insert(170)
-tree.insert(15)
-tree.insert(1)
-console.log(JSON.stringify(traverse(tree.root)));
-
-function traverse(node) {
-  const tree = { value: node.value };
-  tree.left = node.left === null ? null : traverse(node.left);
-  tree.right = node.right === null ? null : traverse(node.right);
-  return tree;
-}
-
-/*
-    9
-  4   20
-1  6 15 170
-*/
