5fc4de81bd2c0252292e046feee62b04ef157d7e
Julian <jthrow00@gmail.com>
2020-07-24 03:59:24 -0700

    Modified factorial functions to be more efficient

--- udemy-dsa/factorial.js
+++ udemy-dsa/factorial.js
@@ -1,10 +1,10 @@
 function findFactorialRecursive(number) {
-  if (number < 2) return number;
+  if (number < 3) return number;
   return number * findFactorialRecursive(--number);
 }
 
 function findFactorialIterative(number) {
-  if (number < 2) return number;
+  if (number < 3) return number;
   let answer = 1;
   for (let i = number; i > 1; i--) {
     answer *= i;
