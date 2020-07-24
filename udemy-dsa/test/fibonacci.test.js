af6d5a660ca933c31d26c7fbb0cd33e22928085f
Julian <jthrow00@gmail.com>
2020-07-24 05:14:06 -0700

    Modified fibo recursive function name

--- udemy-dsa/test/fibonacci.test.js
+++ udemy-dsa/test/fibonacci.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { fibonacciIterativeRecursive, fibonacciIterative } = require('../fibonacci');
+const { fibonacciRecursive, fibonacciIterative } = require('../fibonacci');
 
 describe('fibonacciIterative()', () => {
   context('number is 0', () => {
@@ -23,24 +23,24 @@ describe('fibonacciIterative()', () => {
   });
 });
 
-describe('fibonacciIterativeRecursive()', () => {
+describe('fibonacciRecursive()', () => {
   context('number is 0', () => {
     it('should return 0', () => {
-      expect(fibonacciIterativeRecursive(0)).to.equal(0);
+      expect(fibonacciRecursive(0)).to.equal(0);
     });
   });
   context('number is 1', () => {
     it('should return 1', () => {
-      expect(fibonacciIterativeRecursive(1)).to.equal(1);
+      expect(fibonacciRecursive(1)).to.equal(1);
     });
   });
   context('number is a positive integer > 1', () => {
     it('should return fibonacci number at that index', () => {
-      expect(fibonacciIterativeRecursive(2)).to.equal(1);
-      expect(fibonacciIterativeRecursive(3)).to.equal(2);
-      expect(fibonacciIterativeRecursive(5)).to.equal(5);
-      expect(fibonacciIterativeRecursive(8)).to.equal(21);
-      expect(fibonacciIterativeRecursive(14)).to.equal(377);
+      expect(fibonacciRecursive(2)).to.equal(1);
+      expect(fibonacciRecursive(3)).to.equal(2);
+      expect(fibonacciRecursive(5)).to.equal(5);
+      expect(fibonacciRecursive(8)).to.equal(21);
+      expect(fibonacciRecursive(14)).to.equal(377);
     });
   });
 });
