660019c9346051f858668fa121d67c50d996fcee
Julian <jthrow00@gmail.com>
2020-07-30 04:14:17 -0700

    Modified bubble sort

--- udemy-dsa/bubbleSort.js
+++ udemy-dsa/bubbleSort.js
@@ -1,10 +1,10 @@
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
-    for (let k = i + 1; k < arr.length; k++) {
-      if (arr[i] > arr[k]) {
-        const temp = arr[i];
-        arr[i] = arr[k];
-        arr[k] = temp;
+    for (let k = 0; k < arr.length - 1 - i; k++) {
+      if (arr[k] > arr[k + 1]) {
+        const temp = arr[k];
+        arr[k] = arr[k + 1];
+        arr[k + 1] = temp;
       }
     }
   }
