8d9ed36305a1a12e989f3f783a2468da5977a489
Julian <jthrow00@gmail.com>
2020-08-13 12:51:06 -0700

    Implemented fibonacci with memoization

--- udemy-dsa/fibonacci.js
+++ udemy-dsa/fibonacci.js
@@ -15,5 +15,16 @@ function fibonacciRecursive(n) {
   return fibonacciRecursive(n - 2) + fibonacciRecursive(n - 1);
 }
 
+function fibonacciMemoization(n) {
+  let cache = {};
+  return (function inner(n) {
+    if (n < 2) return n;
+    if (cache[n]) return cache[n];
+    return inner(n - 1) + inner(n - 2);
+  })(n);
+}
+
+console.log(fibonacciMemoization(7));
+
 exports.fibonacciIterative = fibonacciIterative;
 exports.fibonacciRecursive = fibonacciRecursive;
